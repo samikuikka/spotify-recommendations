da2dafacc30419e87f3eabb96e23c24389625e76
Tomi Lahti <tomi.lahti2@gmail.com>
2024-02-03 16:35:46 +0200

    a1

--- app/api/recommendations/route.tsx
+++ app/api/recommendations/route.tsx
@@ -7,6 +7,7 @@ import {
 import { z } from "zod";
 
 import genresList from "@/lib/genres.json";
+import { list } from "postcss";
 
 const openai = new OpenAI({
   apiKey: process.env.OPEN_AI_KEY,
@@ -82,8 +83,58 @@ export async function POST(req: Request, res: Response) {
     // Genres are valid
     const validGenres = genreResult.data;
 
-    console.log(validGenres);
-    return NextResponse.json(chatRes);
+    // Now i want to get artists from theses different genres that have been provided as a list, use gpt again to get artists from the different genres. Make so that it is in a list format at the end. Take insporation from the soution above.
+
+    const genreArtist = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `
+            Your job is to recommend give one artist name from each genre.
+            Identify best artists names to recommend.
+            Available genres are: ${validGenres.join(", ")}
+        `,
+        },
+        {
+          role: "user",
+          content: `Give one artist name from each genre: ${validGenres.join(
+            ", "
+          )}`,
+        },
+      ],
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "list_of_artists",
+            description:
+              "Retrrieves 3-5 artists from the genre list based on the user's input",
+            parameters: {
+              type: "object",
+              properties: {
+                artists: {
+                  type: "array",
+                  items: {
+                    type: "string",
+                    description: "The artist name",
+                  },
+                  minItems: 3,
+                  maxItems: 5,
+                },
+              },
+            },
+          },
+        },
+      ],
+      tool_choice: "auto",
+    });
+
+    const list = JSON.parse(genreArtist.choices[0].message.tool_calls![0].function.arguments).artists.slice(0,5);
+
+    console.log(list);
+
+    return NextResponse.json({ genres: validGenres, artists: genreArtist });
   } catch (e) {
     console.log(e);
     return new NextResponse("Internal Server Error", { status: 500 });
